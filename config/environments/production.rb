1fba207d9db631462ce6b614130168af9cae539f
Sascha Marquardt <sascha@marquardt.koeln>
2015-09-06 17:51:35 +0200

    modified s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,7 @@ Rails.application.configure do
 
 config.paperclip_defaults = {
   :storage => :s3,
-  :s3_endpoint => 'eu-central-1.amazonaws.com'
-  :path => ":class/:id.:style.:extension",
+  :s3_endpoint => 'eu-central-1.amazonaws.com',
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
