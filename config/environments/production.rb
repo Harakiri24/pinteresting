62eaddba43c409ac526265c5bd4e9fb0a84116c7
Sascha Marquardt <sascha@marquardt.koeln>
2015-09-06 16:06:25 +0200

    amazon s3 für paperclip upload hinzugefügt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { host: 'cottoneyejoe.herokuapp.com'}
     Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
 Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
