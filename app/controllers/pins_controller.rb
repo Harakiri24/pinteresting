9d7a7a2765cd57371904853276568b2292c8d2a3
Sascha Marquardt <sascha@marquardt.koeln>
2015-09-06 16:50:59 +0200

    production env bearbeitet 2

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -17,7 +17,7 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.build(@pin_params)
       if @pin.save
        redirect_to @pin, alert: 'Dein Pin wurde erstellt.'
       else
