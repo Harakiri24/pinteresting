da0dfb59a642184dd84260351665cefe2358988a
Sascha Marquardt <sascha@marquardt.koeln>
2015-09-09 08:14:37 +0200

    Fehlerbeseitigung

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,17 +13,4 @@ protected
    devise_parameter_sanitizer.for(:sign_up) << :name
    devise_parameter_sanitizer.for(:account_update) << :name
  end
-
-
-def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 end
